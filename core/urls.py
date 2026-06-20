a1c213aa2ae5ba7cb43fe7e5918c8b2c332e91e5
bdrejo <baderaq@gmail.com>
2026-06-21 00:13:23 +0300

    fix big

--- core/urls.py
+++ core/urls.py
@@ -9,24 +9,12 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('submit/', views.submit_project, name='submit_project'),
-
-    # صفحة الأخبار
     path('news/', views.news_list, name='news_list'),
     path('news/<int:pk>/', views.news_detail, name='news_detail'),
-
-    # صفحة الصور
     path('photos/', views.photos_page, name='photos_page'),
-
-    # مكتبة الفيديو
     path('videos/', views.videos_page, name='videos_page'),
-
-    # قصص النجاح
     path('success-stories/', views.success_stories_page, name='success_stories_page'),
-
-    # الإحصائيات
     path('statistics/', views.statistics_page, name='statistics_page'),
-
-    # الفائزون
     path('winners/', views.winners_page, name='winners_page'),
 ]
 
